85794ec6335659dbddcc3ec371125b533afd44df
chetang12 <102719631+chetang12@users.noreply.github.com>
2022-06-13 14:42:48 +0530

    middleware assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,31 +3,15 @@ const UserModel= require("../models/userModel")
 
 
 
-const basicCode= async function(req, res) {
+const basicCode= async function(req, res,next) {
     let tokenDataInHeaders= req.headers.token
     console.log(tokenDataInHeaders)
 
     console.log( "HEADER DATA ABOVE")
     console.log( "hey man, congrats you have reached the Handler")
     res.send({ msg: "This is coming from controller (handler)"})
+    
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const createUser= async function (req, res) {
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,4 +1,5 @@
-
+const moment = require('moment');
+const  stack = require('stackjs');
 const mid1= function ( req, res, next) {
     req.falana= "hi there. i am adding something new to the req object"
     console.log("Hi I am a middleware named Mid1")
@@ -10,17 +11,30 @@ const mid2= function ( req, res, next) {
     next()
 }
 
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
+
 
 const mid4= function ( req, res, next) {
     console.log("Hi I am a middleware named Mid4")
     next()
 }
 
+const middleware= function(req,res,next){
+
+var currentdate = new Date();
+var datetime = "Last Sync: " + currentdate.getDate() + "/" +
+    (currentdate.getMonth() + 1) + "/" +
+    currentdate.getFullYear() + " @ " +
+    currentdate.getHours() + ":" +
+    currentdate.getMinutes() + ":" +
+    currentdate.getSeconds();
+let ip = req.originalUrl
+let url = req.originalUrl
+console.log(`${datetime} ${ip} ${url}`)
+next()
+}
+
+
 module.exports.mid1= mid1
 module.exports.mid2= mid2
-module.exports.mid3= mid3
 module.exports.mid4= mid4
+module.exports.middleware= middleware
